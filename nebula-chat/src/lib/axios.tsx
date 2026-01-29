225c9f64caa8d03135461edce51602b81ec106f0
Nexxer-cmd <oarjun790@gmail.com>
2026-01-29 12:25:37 +0530

    Update axios.tsx

--- nebula-chat/src/lib/axios.tsx
+++ nebula-chat/src/lib/axios.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const BASE_URL = import.meta.env.MODE === "development" 
   ? "http://localhost:5000" 
-  : "https://nebula-p0u4.onrender.com";
+  : "https://nebula-server-dps6.onrender.com";
 
 export const axiosInstance = axios.create({
   baseURL: `${BASE_URL}/api`,
