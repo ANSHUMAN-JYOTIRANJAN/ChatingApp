98820592d6889665bddc862bad352505a96466cc
ANSHUMAN-JYOTIRANJAN <ansumanswain456@gmail.com>
2026-02-15 00:10:00 +0530

    changes in backend part

--- nebula-chat-backend/server.js
+++ nebula-chat-backend/server.js
@@ -1519,12 +1519,14 @@ const transporter = nodemailer.createTransport({
 });
 
 // --- PASSPORT STRATEGY ---
+console.log("GOOGLE_CALLBACK_URL:", process.env.GOOGLE_CALLBACK_URL);
+
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL, // VERY IMPORTANT
       proxy: true,
     },
     async (token, refToken, profile, done) => {
